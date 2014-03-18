f0761c40ca74611633bd431d83f9ca20cc23efd7
Manjit Singh <manjitsingh23@gmail.com>
2014-03-18 23:22:23 +0000

    enabled sinatra sessions and added /solution

--- sudoku.rb
+++ sudoku.rb
@@ -2,6 +2,8 @@ require 'sinatra'
 require_relative './lib/sudoku'
 require_relative './lib/cell'
 
+enable :sessions
+
 def random_sudoku
   seed = (1..9).to_a.shuffle + Array.new(81-9, 0)
   sudoku = Sudoku.new(seed.join) 
@@ -9,7 +11,18 @@ def random_sudoku
   sudoku.to_s.chars
 end
 
+def puzzle(sudoku)
+  sudoku
+end
+
 get '/' do
+  sudoku = random_sudoku
+  session[:solution] = sudoku
   @current_solution = random_sudoku
+  erb :index
+end
+
+get '/solution' do
+  @current_solution = session[:solution]
   erb :index
 end
