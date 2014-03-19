6a1d0b837c4558e94c0f624081d1daebacc845f0
Manjit Singh <manjitsingh23@gmail.com>
2014-03-19 11:51:52 +0000

    checking the solution implemented

--- sudoku.rb
+++ sudoku.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require_relative './lib/sudoku'
 require_relative './lib/cell'
+require_relative './helpers/application'
 
 enable :sessions
 
@@ -12,17 +13,59 @@ def random_sudoku
 end
 
 def puzzle(sudoku)
+  #need to implement code here
   sudoku
 end
 
 get '/' do
+  prepare_to_check_solution
+  generate_new_puzzle_if_necessary
+  @current_solution = session[:current_solution] || session[:puzzle]
+  @solution = session[:solution]
+  @puzzle = session[:puzzle]
+  erb :index
+end
+
+def generate_new_puzzle_if_necessary
+  return if session[:current_solution] && session[:solution] && session[:puzzle]
   sudoku = random_sudoku
   session[:solution] = sudoku
-  @current_solution = random_sudoku
-  erb :index
+  session[:puzzle] = puzzle(sudoku)
+  session[:current_solution] = session[:puzzle]    
+end
+
+post '/' do
+  cells = (params["cell"])  
+  session[:current_solution] = cells.map{|value| value.to_i }.join
+  session[:check_solution] = true
+  redirect to("/")
 end
 
 get '/solution' do
   @current_solution = session[:solution]
   erb :index
 end
+
+def prepare_to_check_solution
+  @check_solution = session[:check_solution]
+  session[:check_solution] = nil
+end
+
+def box_order_to_row_order(cells)
+  boxes = cells.each_slice(9).to_a
+  (0..8).to_a.inject([]) {|memo, i|
+    first_box_index = i / 3 * 3
+    three_boxes = boxes[first_box_index, 3]
+    three_rows_of_three = three_boxes.map do |box| 
+      row_number_in_a_box = i % 3
+      first_cell_in_the_row_index = row_number_in_a_box * 3
+      box[first_cell_in_the_row_index, 3]
+    end
+    memo += three_rows_of_three.flatten
+  }
+end
+
+
+
+
+
